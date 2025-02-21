d634a9ffb86beb54dd8352c16f81f0c74d82f86c
OscarBarrera89 <oscarbar745504@gmail.com>
2025-02-21 13:42:50 +0100

    cambio en el confi.js

--- back-end/config/config.js
+++ back-end/config/config.js
@@ -4,13 +4,13 @@ require('dotenv').config({
 
 
 module.exports = {
-port: process.env.PORT || 80,
+port: process.env.PORT || 3000,
 db: {
-  host: process.env.DB_HOST || "switchback.proxy.rlwy.net",
+  host: process.env.DB_HOST || "localhost",
   user: process.env.DB_USER || "root",
-  password: process.env.DB_PASSWORD || "xlPQUuhLrqywEvFuPSsLGFLedRvQNpQz",
+  password: process.env.DB_PASSWORD || "test",
   name: process.env.DB_NAME || "ventaCoches",
-  port: process.env.DB_PORT || 25751,
+  port: process.env.DB_PORT || 3006,
 },
 secretKey: process.env.SECRET_KEY || "default_secret",
 };
